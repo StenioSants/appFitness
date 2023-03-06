ca154626b9f9539e475eec7af1ce7463a2bf8306
Stenio Sants <stenio.neo@gmail.com>
2023-03-05 21:56:28 -0300

    interface inicial do app

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,11 +1,38 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var btnloguin: Button
+    private lateinit var btncadastrar: TextView
+    private lateinit var btnrecuperar: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnloguin = findViewById(R.id.btn_loguin)
+        btnloguin.setOnClickListener {
+            val i = Intent(this, MainItem::class.java )
+            startActivity(i)
+        }
+        btncadastrar = findViewById(R.id.cadastro)
+        btncadastrar.setOnClickListener {
+            val i = Intent(this, Casdatrar::class.java )
+            startActivity(i)
+        }
+        btnrecuperar = findViewById(R.id.recuperar)
+        btnrecuperar.setOnClickListener {
+            val i = Intent(this, RecuperarSenha::class.java )
+            startActivity(i)
+        }
+
+
     }
 }
---
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainItem.kt
@@ -0,0 +1,11 @@
+package co.tiagoaguiar.fitnesstracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainItem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_item)
+    }
+}
