0f2c24796786f7624772858f1a5ec531696b9247
Stenio Sants <stenio.neo@gmail.com>
2023-03-10 00:00:52 -0300

    refazendo interface vertical/horizontal

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -9,29 +9,29 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var btnloguin: Button
-    private lateinit var btncadastrar: TextView
-    private lateinit var btnrecuperar: TextView
+//    private lateinit var btnloguin: Button
+//    private lateinit var btncadastrar: TextView
+//    private lateinit var btnrecuperar: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnloguin = findViewById(R.id.btn_loguin)
-        btnloguin.setOnClickListener {
-            val i = Intent(this, MainItem::class.java )
-            startActivity(i)
-        }
-        btncadastrar = findViewById(R.id.cadastro)
-        btncadastrar.setOnClickListener {
-            val i = Intent(this, Casdatrar::class.java )
-            startActivity(i)
-        }
-        btnrecuperar = findViewById(R.id.recuperar)
-        btnrecuperar.setOnClickListener {
-            val i = Intent(this, RecuperarSenha::class.java )
-            startActivity(i)
-        }
+//        btnloguin = findViewById(R.id.btn_loguin)
+//        btnloguin.setOnClickListener {
+//            val i = Intent(this, MainItem::class.java )
+//            startActivity(i)
+//        }
+//        btncadastrar = findViewById(R.id.cadastro)
+//        btncadastrar.setOnClickListener {
+//            val i = Intent(this, Casdatrar::class.java )
+//            startActivity(i)
+//        }
+//        btnrecuperar = findViewById(R.id.recuperar)
+//        btnrecuperar.setOnClickListener {
+//            val i = Intent(this, RecuperarSenha::class.java )
+//            startActivity(i)
+//        }
 
 
     }
